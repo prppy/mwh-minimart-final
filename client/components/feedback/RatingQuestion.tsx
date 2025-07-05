cd9b821ab3671741af202ac4354ad7ab3a76f513
myathetchai <myathetchai441@gmail.com>
2025-07-05 16:40:05 +0800

    (style):adjusted feedback selection colors

--- client/components/feedback/RatingQuestion.tsx
+++ client/components/feedback/RatingQuestion.tsx
@@ -1,58 +1,3 @@
-// // RatingQuestion.tsx
-// import React from "react";
-// import { Box, Text, HStack } from "@gluestack-ui/themed";
-// import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-
-// export type RatingOption = "happy" | "neutral" | "sad" | null;
-
-// const RatingColors = {
-//   sad: "#D5442A",
-//   happy: "#60D52A",
-//   neutral: "#FF8B37",
-// };
-
-// interface RatingQuestionProps {
-//   question: string;
-//   value: RatingOption;
-//   onSelect: (value: RatingOption) => void;
-// }
-
-// const RatingQuestion: React.FC<RatingQuestionProps> = ({
-//   question,
-//   value,
-//   onSelect,
-// }) => {
-//   return (
-//     <Box mt="$4">
-//       <Text fontSize="$xl" fontWeight="$medium" mb="$5">
-//         {question}
-//       </Text>
-//       <HStack space="xl" justifyContent="space-around">
-//         <MaterialIcons
-//           name="sentiment-very-satisfied"
-//           size={50}
-//           color={value === "happy" ? RatingColors.happy : "#ccc"}
-//           onPress={() => onSelect("happy")}
-//         />
-//         <MaterialIcons
-//           name="sentiment-neutral"
-//           size={50}
-//           color={value === "neutral" ? RatingColors.neutral : "#ccc"}
-//           onPress={() => onSelect("neutral")}
-//         />
-//         <MaterialIcons
-//           name="sentiment-very-dissatisfied"
-//           size={50}
-//           color={value === "sad" ? RatingColors.sad : "#ccc"}
-//           onPress={() => onSelect("sad")}
-//         />
-//       </HStack>
-//     </Box>
-//   );
-// };
-
-// export default RatingQuestion;
-// RatingQuestion.tsx
 import React from "react";
 import { Box, Text, HStack } from "@gluestack-ui/themed";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
@@ -97,14 +42,14 @@ const RatingQuestion: React.FC<RatingQuestionProps> = ({
           const color = RatingColors[option];
           const isSelected = value === option;
           const darkerBg = isSelected
-            ? setHSLlightness(color, 20)
+            ? setHSLlightness(color, 90)
             : "transparent";
 
           return (
             <Box
               key={option}
               borderRadius={100}
-              bg={hslToHSLA(darkerBg, isSelected ? 0.1 : 0)}
+              bg={darkerBg}
               style={{
                 borderColor: isSelected ? color : "transparent",
                 borderWidth: isSelected ? 1 : 0,
